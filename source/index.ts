e0d2debb4efc703304c877f2f53a4c4d937822ca
puneeth-techie <puneethpgowda.techie@gmail.com>
2021-04-14 17:40:29 +0530

    Implemented logging and server.

---
+++ source/index.ts
@@ -0,0 +1,14 @@
+import http from 'http'
+import app from '@startup/app'
+import config from '@config/config'
+import logging from '@config/logging'
+
+const NAMESPACE = 'Server';
+
+/**Initializing the server */
+const server = http.createServer(app)
+
+/**Starting server */
+server.listen(config.server.port, () => {
+    logging.info(NAMESPACE, `Server is running ${config.server.hostname}: ${config.server.port}`);
+})
---
+++ source/startup/app.ts
@@ -0,0 +1,36 @@
+import express from 'express'
+import logging from '@config/logging';
+
+/** Init express */
+const app = express()
+const NAMESPACE = 'Server'
+
+/** Log the request handler info */
+app.use((req, res, next) => {
+    /** Log the req */
+    logging.info(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - IP: [${req.socket.remoteAddress}]`);
+    res.on('finish', () => {
+        /**Log the res */
+        logging.info(NAMESPACE, `METHOD: [${req.method}] - URL: [${req.url}] - STATUS: [${req.statusCode}] - IP: [${req.socket.remoteAddress}]`)
+    })
+    next()
+})
+
+/** Parse the body of the request */
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+/** Rules of our API */
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method == 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+
+    next();
+});
+
+export default app;
